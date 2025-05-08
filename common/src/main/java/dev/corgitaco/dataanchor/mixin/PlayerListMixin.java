3d5e812733ea54185527f1fa430e4e71fd179bf7
Corgi Taco <66983020+CorgiTaco@users.noreply.github.com>
2025-05-07 21:39:55 -0700

    Fix timing issues when respawning

--- common/src/main/java/dev/corgitaco/dataanchor/mixin/PlayerListMixin.java
+++ common/src/main/java/dev/corgitaco/dataanchor/mixin/PlayerListMixin.java
@@ -13,25 +13,26 @@ import dev.corgitaco.dataanchor.data.registry.TrackedDataKey;
 import dev.corgitaco.dataanchor.data.type.entity.PlayerTrackedData;
 import dev.corgitaco.dataanchor.data.type.entity.ServerPlayerTrackedData;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.players.PlayerList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Collection;
 
-@Mixin(ServerPlayer.class)
-public class ServerPlayerMixin {
+@Mixin(PlayerList.class)
+public class PlayerListMixin {
 
 
-    @Inject(method = "restoreFrom", at = @At("TAIL"))
-    private void dataAnchor$restoreFrom(ServerPlayer oldPlayer, boolean keepEverything, CallbackInfo ci) {
+    @Inject(method = "respawn", at = @At("TAIL"))
+    private void dataAnchor$restoreFrom(ServerPlayer oldPlayer, boolean keepEverything, CallbackInfoReturnable<ServerPlayer> cir) {
         if (this instanceof TrackedDataContainer access) {
             Collection<TrackedDataKey<ServerPlayerTrackedData>> keys = access.dataAnchor$getTrackedDataKeys();
             keys.forEach(key -> {
                 access.dataAnchor$getTrackedData(key).ifPresent(data -> {
                     if (data instanceof PlayerTrackedData playerTrackedData) {
-                        playerTrackedData.copy(oldPlayer, keepEverything);
+                        playerTrackedData.respawn(oldPlayer, keepEverything);
                     }
                 });
             });
